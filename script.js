e8d3e6db75c5e6acf3ab18934b91d423c4998612
Siang Huang <sianglifen@gmail.com>
2025-11-18 16:28:32 +0800

    feat: json loader

---
+++ script.js
@@ -0,0 +1,73 @@
+const diedTableGenerator = ((object) => {
+    const index = object['index'];
+    const name = object['name'];
+    const from = object['from'];
+    const joinDate = object['join-date'];
+    const diedDate = object['died-date'];
+    const diedReason = object['died-reason'];
+    const contribution = object['contribution'];
+    const hurt = object['hurt'];
+
+    let status_text_ele;
+    if (diedDate == null) {
+        status_text_ele = hurt 
+            ? '<span style="color: orange;">受傷</span>' 
+            : '<span style="color: black;">服役中</span>';
+    } else {
+        status_text_ele = '<span style="color: red;">陣亡</span>';
+    }
+
+    return `    
+        <tr>
+            <td>${index}</td>
+            <td><a href="#died-${index}">${name}</a></td>
+            <td>${status_text_ele}</td>
+        </tr>
+    `
+})
+
+const diedBoardGenerator = ((object) => {    
+    const index = object['index'];
+    const name = object['name'];
+    const from = object['from'];
+    const joinDate = object['join-date'];
+    const diedDate = object['died-date'];
+    const diedReason = object['died-reason'];
+    const contribution = object['contribution'];
+    const hurt = object['hurt'];
+
+    let status_text_ele;
+    if (diedDate == null) {
+        status_text_ele = hurt 
+            ? '<p style="color: orange;">受傷</p>' 
+            : '<p style="color: black;">服役中</p>';
+    } else {
+        status_text_ele = '<p style="color: red;">陣亡</p>';
+    }
+
+    return `    
+    <div class="died-area" id="died-${index}">
+        <img class="died-img" src="assets/umbrella/${index}.jpg" alt="傘${index}" onerror="this.src='assets/umbrella.png'; this.onerror=null;">
+        <p>${name}</p>
+        <b>${status_text_ele}</b>
+        <p>籍貫：${from}</p>
+        <p>加入日期：${joinDate}</p>
+        ${
+            diedDate != null ? `<p>逝世日期：${diedDate}</p>` : ``
+        }
+    </div>
+    `
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const diedTable = document.getElementById('died-table')?.getElementsByTagName('tbody')[0];
+    const diedBoard = document.getElementById('died-board') ?? null;
+    fetch('list.json')
+        .then(response => response.json())
+        .then(data => {
+            data.umbrella.forEach(object => {
+                if (diedTable) diedTable.innerHTML += diedTableGenerator(object);
+                if (diedBoard) diedBoard.innerHTML += diedBoardGenerator(object);
+            });
+    })
+});
